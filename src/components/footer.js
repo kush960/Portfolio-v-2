4a8df5a7e1ff50e1c88048a27d153f543c3e3c54
Kushagra Ankit <Kushagraankit05@gmail.com>
2021-03-16 21:08:21 +0530

    footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -105,7 +105,7 @@ const Footer = () => {
       </StyledSocialLinks>
 
       <StyledCredit tabindex="-1">
-        <a href="https://github.com/bchiang7/v4">
+        <a href="https://github.com/kush960/Portfolio-v-2">
           <div>Designed &amp; Built by Kushagra Ankit</div>
 
           {githubInfo.stars && githubInfo.forks && (
